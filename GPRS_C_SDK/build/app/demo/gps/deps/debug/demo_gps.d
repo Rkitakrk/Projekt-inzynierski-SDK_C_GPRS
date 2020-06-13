1ddd8a93aeafd96717a171500682566026e61ac8
Rkitakrk <kita.rafal.m@gmail.com>
2020-06-13 16:20:42 +0200

    first commit

---
+++ GPRS_C_SDK/build/app/demo/gps/deps/debug/demo_gps.d
@@ -0,0 +1,133 @@
+C:/GPRS_C_SDK/build/app/demo/gps/obj/debug/demo_gps.o: src/demo_gps.c \
+ C:/GPRS_C_SDK/include/std_inc/string.h \
+ C:/GPRS_C_SDK/include/std_inc/stddef.h \
+ C:/GPRS_C_SDK/include/std_inc/cs_types.h \
+ C:/GPRS_C_SDK/include/std_inc/stdbool.h \
+ C:/GPRS_C_SDK/include/std_inc/stdio.h \
+ C:/GPRS_C_SDK/include/std_inc/stdarg.h C:/GPRS_C_SDK/include/sdk_init.h \
+ C:/GPRS_C_SDK/include/std_inc/cs_types.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_fs.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_gpio.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_uart.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_network.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_socket.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_os.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_sim.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_sms.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_time.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_pm.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_call.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_adc.h \
+ C:/GPRS_C_SDK/include/std_inc/stdint.h \
+ C:/GPRS_C_SDK/include/std_inc/stdbool.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_mqtt.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_charset.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_i2c.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_ssl.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_spi.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_fota.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_ss.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_gizwits.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_audio.h \
+ C:/GPRS_C_SDK/include/api_os.h C:/GPRS_C_SDK/include/api_gps.h \
+ C:/GPRS_C_SDK/include/api_inc/api_event.h \
+ C:/GPRS_C_SDK/include/api_hal_uart.h C:/GPRS_C_SDK/include/api_debug.h \
+ C:/GPRS_C_SDK/include/sdk_init.h \
+ C:/GPRS_C_SDK/libs/utils/include/buffer.h \
+ C:/GPRS_C_SDK/libs/gps/include/gps_parse.h \
+ C:/GPRS_C_SDK/libs/gps/minmea/src/minmea.h \
+ C:/GPRS_C_SDK/include/std_inc/errno.h \
+ C:/GPRS_C_SDK/include/std_inc/time.h \
+ C:/GPRS_C_SDK/include/std_inc/math.h \
+ C:/GPRS_C_SDK/include/std_inc/stdlib.h \
+ C:/GPRS_C_SDK/libs/gps/include/gps.h
+
+C:/GPRS_C_SDK/include/std_inc/string.h:
+
+C:/GPRS_C_SDK/include/std_inc/stddef.h:
+
+C:/GPRS_C_SDK/include/std_inc/cs_types.h:
+
+C:/GPRS_C_SDK/include/std_inc/stdbool.h:
+
+C:/GPRS_C_SDK/include/std_inc/stdio.h:
+
+C:/GPRS_C_SDK/include/std_inc/stdarg.h:
+
+C:/GPRS_C_SDK/include/sdk_init.h:
+
+C:/GPRS_C_SDK/include/std_inc/cs_types.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_fs.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_gpio.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_uart.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_network.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_socket.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_os.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_sim.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_sms.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_time.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_pm.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_call.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_adc.h:
+
+C:/GPRS_C_SDK/include/std_inc/stdint.h:
+
+C:/GPRS_C_SDK/include/std_inc/stdbool.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_mqtt.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_charset.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_i2c.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_ssl.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_spi.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_fota.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_ss.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_gizwits.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_audio.h:
+
+C:/GPRS_C_SDK/include/api_os.h:
+
+C:/GPRS_C_SDK/include/api_gps.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_event.h:
+
+C:/GPRS_C_SDK/include/api_hal_uart.h:
+
+C:/GPRS_C_SDK/include/api_debug.h:
+
+C:/GPRS_C_SDK/include/sdk_init.h:
+
+C:/GPRS_C_SDK/libs/utils/include/buffer.h:
+
+C:/GPRS_C_SDK/libs/gps/include/gps_parse.h:
+
+C:/GPRS_C_SDK/libs/gps/minmea/src/minmea.h:
+
+C:/GPRS_C_SDK/include/std_inc/errno.h:
+
+C:/GPRS_C_SDK/include/std_inc/time.h:
+
+C:/GPRS_C_SDK/include/std_inc/math.h:
+
+C:/GPRS_C_SDK/include/std_inc/stdlib.h:
+
+C:/GPRS_C_SDK/libs/gps/include/gps.h:
---
+++ GPRS_C_SDK/build/app/libs/utils/deps/debug/api_debug.d
@@ -0,0 +1,99 @@
+C:/GPRS_C_SDK/build/app/libs/utils/obj/debug/api_debug.o: src/api_debug.c \
+ C:/GPRS_C_SDK/include/api_debug.h C:/GPRS_C_SDK/include/sdk_init.h \
+ C:/GPRS_C_SDK/include/std_inc/string.h \
+ C:/GPRS_C_SDK/include/std_inc/stddef.h \
+ C:/GPRS_C_SDK/include/std_inc/cs_types.h \
+ C:/GPRS_C_SDK/include/std_inc/stdbool.h \
+ C:/GPRS_C_SDK/include/std_inc/cs_types.h \
+ C:/GPRS_C_SDK/include/sdk_init.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_fs.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_gpio.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_uart.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_network.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_socket.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_os.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_sim.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_sms.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_time.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_pm.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_call.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_adc.h \
+ C:/GPRS_C_SDK/include/std_inc/stdint.h \
+ C:/GPRS_C_SDK/include/std_inc/stdbool.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_mqtt.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_charset.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_i2c.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_ssl.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_spi.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_fota.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_ss.h \
+ C:/GPRS_C_SDK/include/std_inc/stdarg.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_gizwits.h \
+ C:/GPRS_C_SDK/include/api_inc/api_inc_audio.h \
+ C:/GPRS_C_SDK/include/std_inc/stdio.h
+
+C:/GPRS_C_SDK/include/api_debug.h:
+
+C:/GPRS_C_SDK/include/sdk_init.h:
+
+C:/GPRS_C_SDK/include/std_inc/string.h:
+
+C:/GPRS_C_SDK/include/std_inc/stddef.h:
+
+C:/GPRS_C_SDK/include/std_inc/cs_types.h:
+
+C:/GPRS_C_SDK/include/std_inc/stdbool.h:
+
+C:/GPRS_C_SDK/include/std_inc/cs_types.h:
+
+C:/GPRS_C_SDK/include/sdk_init.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_fs.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_gpio.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_uart.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_network.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_socket.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_os.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_sim.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_sms.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_time.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_pm.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_call.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_adc.h:
+
+C:/GPRS_C_SDK/include/std_inc/stdint.h:
+
+C:/GPRS_C_SDK/include/std_inc/stdbool.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_mqtt.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_charset.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_i2c.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_ssl.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_spi.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_fota.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_ss.h:
+
+C:/GPRS_C_SDK/include/std_inc/stdarg.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_gizwits.h:
+
+C:/GPRS_C_SDK/include/api_inc/api_inc_audio.h:
+
+C:/GPRS_C_SDK/include/std_inc/stdio.h:
